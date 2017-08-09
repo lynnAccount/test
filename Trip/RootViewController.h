3887f30dea80fb53f49183d2872b99141acccd44
方杰 <wb-fangjie@le.com>
2017-08-09 10:18:09 +0800

    添加工程文件

---
+++ Trip/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Trip
+//
+//  Created by JIE on 17/8/9.
+//  Copyright © 2017年 JIE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ Trip/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Trip
+//
+//  Created by JIE on 17/8/9.
+//  Copyright © 2017年 JIE. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
